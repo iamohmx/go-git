1dc4bd914e21ce1b2937ca50fd1a3bf2b4140f2f
iamohmx <48460454+iamohmx@users.noreply.github.com>
2023-08-03 11:43:00 +0700

    -feature :sparkles: sum completed!

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import "fmt"
 
+func sum(a, b int) int {
+	return a + b
+}
+
 func main() {
 	fmt.Println("Hello GitHub from Git Command")
-	fmt.Println("First Git")
+	fmt.Println(sum(2, 8))
 }
